b120e5bf79b74940e2b5afb3dd8bfd79a5fa332d
RoCky Wu <rwu@cepave.com>
2016-09-14 10:29:03 +0800

    Added test examples

--- src/components/select/__test__/select.spec.js
+++ src/components/select/__test__/select.spec.js
@@ -1,6 +1,34 @@
-import Vue from 'vue'
 import Select from '../'
 
-test('Test <Select />', ()=> {
+it('Test <Select /> default `foo`', ()=> {
+  const vm = render((h)=> {
+    return (
+      <Select />
+    )
+  })
 
+  expect(vm.foo).toBe(1)
+})
+
+it('Test <Select /> `handleClick`', ()=> {
+  const vm = render((h)=> {
+    return (
+      <Select />
+    )
+  })
+
+  vm.handleClick()
+  expect(vm.foo).toBe(100)
+})
+
+
+it('Test <Select /> props `name` and `age`', ()=> {
+  const vm = render((h)=> {
+    return (
+      <Select name="Jimy" age={300} />
+    )
+  })
+
+  expect(vm.name).toBe('Jimy')
+  expect(vm.age).toBe(300)
 })
--- src/components/select/index.js
+++ src/components/select/index.js
@@ -2,11 +2,30 @@ import s from './select.scss'
 
 const Select = {
   name: 'Select',
+
+  props: {
+    name: '',
+    age: 0,
+  },
+
+  data() {
+    return {
+      foo: 1,
+    }
+  },
+
+  methods: {
+    handleClick() {
+      this.foo = 100
+    }
+  },
+
   render(h) {
-    const { $slots } = this
+    const { $slots, foo } = this
     return (
       <div class={[s.body]}>
         This is Select component
+        <div data-foo={foo}></div>
       </div>
     )
   }
