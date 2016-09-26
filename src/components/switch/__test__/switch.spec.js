f05139ff1270eb609e99f3762b3caec98f4e3e77
Kelly Su <yoyo420303@gmail.com>
2016-09-26 17:23:10 +0800

    [OWL-1072] test dynamic updates

--- src/components/switch/__test__/switch.spec.js
+++ src/components/switch/__test__/switch.spec.js
@@ -1,23 +1,51 @@
 import Switch from '../'
 
 it('default status should be checked', () => {
-  const vm = render((h)=> {
-    return (
-      <Switch checked={true}/>
-    )
+  const vm = shallow({
+    render(h) {
+      return (
+        <Switch checked={true}/>
+      )
+    }
   })
   expect(vm.check).toBe(true)
 })
 
-it('status and output data should be changed to `false` after one click', () => {
+it('status and output data should be changed to `false` after one click', async() => {
   const switchChange = (data) => {
     expect(Object.values(data)[0]).toBe(false)
   }
-  const vm = render((h) => {
-    return (
-      <Switch checked={true} onChange={switchChange}/>
-    )
+  const vm = shallow({
+    render(h) {
+      return (
+        <Switch checked={true} onChange={switchChange}/>
+      )
+    }
   })
   vm.handleClick()
+  await new Promise((done) => {
+    vm.$nextTick(done)
+  })
+  expect(vm.check).toBe(false)
+})
+
+it('`check` should be changed after dynamic updates', async() => {
+  let vm
+  await new Promise((done) => {
+    vm = shallow({
+      data: {
+        input : true
+      },
+      mounted() {
+        this.input = false
+        this.$nextTick(done)
+      },
+      render(h) {
+        return (
+          <Switch checked={this.input} />
+        )
+      }
+    })
+  })
   expect(vm.check).toBe(false)
 })
