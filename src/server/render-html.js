497e9ef2841f86b40d8e019f7ba6399c8cdc0550
RoCky Wu <rwu@cepave.com>
2016-10-05 14:18:25 +0800

    Added `charset="UTF-8"`

--- src/server/render-html.js
+++ src/server/render-html.js
@@ -1,5 +1,6 @@
 module.exports = ({ appHTML, initState = {} })=> {
   return `<!doctype html>
+<meta charset="UTF-8" />
 <link href="//fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i" rel="stylesheet" />
 <link href="//fonts.googleapis.com/css?family=Source+Code+Pro:400,300,200" rel="stylesheet" />
 <link rel="stylesheet" href="//codemirror.net/theme/neo.css" />
