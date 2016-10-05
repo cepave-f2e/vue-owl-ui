760344ab564ba9ed44687d6c6eee3af73623e795
RoCky Wu <rwu@cepave.com>
2016-10-05 14:44:36 +0800

    `build.ui` includes `.svg` and `.css`

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,22 @@
+const isNode = !(typeof window !== 'undefined' &&
+  window.document &&
+  document.createElement)
+
+if (process.env.NODE_ENV === 'build' && !isNode) {
+  const iconRaw = require('raw!~dist/owl-ui.svg')
+  const classRaw = require('raw!~dist/owl-ui.css')
+
+  const style = document.createElement('style')
+  style.textContent = classRaw
+
+  const div = document.createElement('div')
+  div.innerHTML = iconRaw
+  div.id = 'owl-ui-icons'
+
+  document.head.appendChild(style)
+  document.body.appendChild(div)
+}
+
 module.exports = {
   Select: require('./select')
 }
