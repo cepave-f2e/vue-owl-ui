d2e41cdf52236039ffcf887b18782a8b01a0a633
RoCky Wu <rwu@cepave.com>
2016-10-05 14:50:31 +0800

    Added Icon component and test spec

---
+++ src/components/icon/__test__/icon.spec.js
@@ -0,0 +1,28 @@
+import Icon from '../'
+
+it('set `typ` and `size` props', ()=> {
+  const vm = shallow({
+    render(h) {
+      return (
+        <Icon typ="xyz" size={100} />
+      )
+    }
+  })
+
+  expect(vm.typ).toBe('xyz')
+  expect(vm.$el.getAttribute('width')).toBe('100')
+  expect(vm.$el.getAttribute('height')).toBe('100')
+})
+
+it('set `size` as string', ()=> {
+  const vm = shallow({
+    render(h) {
+      return (
+        <Icon typ="xyz" size={'100x10'} />
+      )
+    }
+  })
+
+  expect(vm.$el.getAttribute('width')).toBe('100')
+  expect(vm.$el.getAttribute('height')).toBe('10')
+})
---
+++ src/components/icon/index.js
@@ -0,0 +1,45 @@
+const Icon = {
+  name: 'Icon',
+
+  props: {
+    typ: {
+      default: '',
+      required: true,
+      type: String,
+    },
+
+    size: {
+      type: [String, Number],
+      default: '20x20'
+    },
+  },
+
+  computed: {
+    cSize() {
+      const { size } = this
+      let width, height
+
+      if (size.includes && size.includes('x')) {
+        ;[width, height] = size.split('x')
+      } else {
+        width = height = size
+      }
+
+      return {
+        width, height
+      }
+    }
+  },
+
+  render(h) {
+    const { typ, cSize } = this
+
+    return (
+      <svg width={cSize.width} height={cSize.height} domProps-innerHTML={
+        `<use xlink:href="#${typ}"></use>`
+      } />
+    )
+  }
+}
+
+module.exports = Icon
