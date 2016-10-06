5ed17f4a0024fbb81f482f3e41728cb0a4da64f4
Timmy Chang <timmy5519@gmail.com>
2016-10-06 16:26:57 +0800

    [OWL-599] add Select component

--- src/components/select/index.js
+++ src/components/select/index.js
@@ -1,31 +1,168 @@
+import delegate from 'delegate-to'
 import s from './select.scss'
 
 const Select = {
   name: 'Select',
 
   props: {
-    name: '',
-    age: 0,
+    isOpen: {
+      type: Boolean,
+      default: false,
+    },
+
+    isDisable: {
+      type: Boolean,
+      default: false,
+    },
+
+    name: {
+      type: String,
+      default: ''
+    },
+
+    onChange: {
+      type: Function,
+    },
+  },
+
+  methods: {
+    close() {
+      this.opened = false
+    },
+
+    toggleMenu() {
+      if (this.isDisable) {
+        return
+      }
+
+      this.opened = !this.opened
+    },
+
+    _handleOnChange: delegate('[data-role="select-option"]', function(ev) {
+      const { delegateTarget } = ev
+      const { $children, d, onChange } = this
+      const idx = +delegateTarget.getAttribute('data-idx')
+
+      if (idx === d.selectedIdx) {
+        return
+      }
+
+      this.d.title = $children[idx].$slots.default
+      this.d.value = $children[idx].value
+
+      this.d.selectedIdx = idx
+      this.opened = false
+
+      onChange({
+        value: $children[idx].value,
+        idx
+      })
+    })
+  },
+
+  watch: {
+    isOpen(newVal) {
+      this.opened = newVal
+    },
   },
 
   data() {
     return {
-      foo: 1,
+      opened: this.isOpen,
     }
   },
 
-  methods: {
-    handleClick() {
-      this.foo = 100
+
+  mounted() {
+
+  },
+
+  computed: {
+    d() {
+      const { $slots } = this
+
+      if (!$slots.default) return {}
+
+      let selectedIdx = 0, selectedVNode = $slots.default[0]
+
+      $slots.default.forEach((v, idx) => {
+        if (typeof v.data.attrs === 'undefined') {
+          v.data.attrs = {}
+        }
+        v.data.attrs['data-idx'] = idx
+
+        if (v.componentOptions.propsData.selected) {
+          selectedIdx = idx
+          selectedVNode = v
+        }
+      })
+      return {
+        title: selectedVNode.componentOptions.children,
+        value: selectedVNode.componentOptions.propsData.value,
+        selectedIdx
+      }
+    },
+
+    css() {
+      const { opened, isDisable } = this
+      const style = {}
+      style[s.selectOpen] = opened
+      style[s.disabled] = isDisable
+      return style
     }
   },
 
   render(h) {
-    const { $slots, foo } = this
+    const { close, $slots, name, _handleOnChange,
+            toggleMenu, d, css } = this
+
     return (
-      <div class={[s.body]}>
-        This is Select component
-        <div data-foo={foo}></div>
+      <div class={[s.selecter, css]} on-blur={close} tabindex="-1">
+        <div class={[s.selectTitle]} on-click={toggleMenu}>
+          <div class={[s.titleText]}>
+            {d.title}
+          </div>
+
+          <div class={[s.titleRight]}>
+            <div class={[s.arrow]}></div>
+          </div>
+        </div>
+
+        <div class={[s.optionBox, css]} on-click={_handleOnChange}>
+          {$slots.default}
+        </div>
+        <input type="hidden" name={name} value={d.value} />
+      </div>
+    )
+  },
+}
+
+Select.Option = {
+  name: 'SelectOption',
+  props: {
+    selected: {
+      type: Boolean,
+      default: false,
+    },
+
+    value: {
+      type: String,
+      default: ''
+    }
+  },
+
+  data() {
+    return {
+
+    }
+  },
+
+  render(h) {
+    const { $slots } = this
+
+    return (
+      <div class={[]} data-role="select-option">
+        {$slots.default}
       </div>
     )
   }
