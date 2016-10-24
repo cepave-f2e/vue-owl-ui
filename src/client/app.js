e2caaba9bf7ee28520038fb7605185b966a20043
Kelly Su <yoyo420303@gmail.com>
2016-10-24 11:46:44 +0800

    [OWL-939] [OWL-940] fix /router urls and left column links

--- src/client/app.js
+++ src/client/app.js
@@ -54,8 +54,10 @@ module.exports = new Vue({
                 <router-link to="/home">Home</router-link>
               </li>
               <li>Select</li>
-              <li>Checkbox</li>
               <li>Input</li>
+              <li>
+                <router-link to="/icons">Icon</router-link>
+              </li>
               <li>
                 <router-link to="/loading">Loading</router-link>
               </li>
--- src/client/router.js
+++ src/client/router.js
@@ -11,12 +11,9 @@ module.exports = new Router({
     { path: '/loading', component: require('./views/loading') },
     { path: '/switch', component: require('./views/switch') },
     { path: '/icons', component: require('./views/icons') },
-<<<<<<< 418b4a07bae11fd33a3f3dd2126a2386687c5c27
     { path: '/tip', component: require('./views/tip') },
     { path: '/checkbox', component: require('./views/checkbox') },
-=======
     { path: '/radio', component: require('./views/radio') },
->>>>>>> [OWL-939] [OWL-940] <Radio> doc
     { path: '*', redirect: '/home' },
   ],
 })
