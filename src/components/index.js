0f6b90e03a2714075b9262b0078c15ac1f9b3639
RoCky Wu <rwu@cepave.com>
2016-11-08 16:51:57 +1100

    Separate out `is-env`

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-const isNode = !(typeof window !== 'undefined' && window.document && document.createElement)
+import { isBrowser } from './is-env'
 
-if (process.env.NODE_ENV === 'build' && !isNode) {
+if (process.env.NODE_ENV === 'build' && isBrowser) {
   const style = document.createElement('style')
   style.textContent = require('raw!~dist/owl-ui.css')
 
