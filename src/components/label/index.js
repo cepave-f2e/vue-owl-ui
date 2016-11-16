e7536b3b8f4be86019be01cc29266fd291adc35b
Kelly Su <yoyo420303@gmail.com>
2016-11-16 17:47:18 +0800

    [OWL-1216] <Label/> component

---
+++ src/components/label/index.js
@@ -0,0 +1,46 @@
+import s from './label.scss'
+
+const Label = {
+  name: 'Label',
+  props: {
+    typ: {
+      type: String,
+      default: ''//label, outline, tag
+    },
+    status: {
+      type: String,
+      default: ''//default, primary, success, error, warning, inverted
+    },
+    badge: {
+      type: Boolean,
+      default: false
+    }
+  },
+  computed: {
+    style() {
+      const { status, typ, badge } = this
+      const style = [s.label]
+      if (typ) {
+        style.push(s[typ])
+      }
+      if (status) {
+        const type = (typ === 'badge') ? '' : typ
+        style.push(s[`${status}${type}`])
+      }
+      if (badge) {
+        style.push(s.badge)
+      }
+      return style
+    }
+  },
+  render(h) {
+    const { $slots, style } = this
+    return (
+      <div class={style}>
+        {$slots.default}
+      </div>
+    )
+  }
+}
+
+module.exports = Label
