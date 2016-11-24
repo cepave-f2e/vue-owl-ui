77e6863435f872cccb1e57274f8aa65f4cb80b7d
RoCky Wu <rocky823@gmail.com>
2016-11-24 18:24:55 +1100

    Clean the shallow render

--- test/setup.js
+++ test/setup.js
@@ -1,15 +1,7 @@
 import Vue from 'vue'
 
-const { body } = document
-const div = document.createElement('div')
-div.id = 'jest'
-
-body.appendChild(div)
-
 window.shallow = (o = {})=> {
-  div.innerHTML = `<div id="test"></div>`
-  const vm = new Vue(o).$mount('#test')
-
+  const vm = new Vue(o).$mount()
   return vm.$children[0]
 }
 
