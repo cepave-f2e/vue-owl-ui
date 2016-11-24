566dd779662d52b4ad006538bde92750e0f35651
Kelly Su <yoyo420303@gmail.com>
2016-11-24 14:32:17 +0800

    [OWL-941][OWL-942] doc: change to use `$emit` and `onChange`

--- src/client/views/checkbox/index.js
+++ src/client/views/checkbox/index.js
@@ -3,13 +3,29 @@ import Markdown from '../../components/markdown'
 
 const CheckboxPage = {
   name: 'CheckboxPage',
-
+  data() {
+    return {
+      checkedDatum: { 1: true, 2: true, all: true }
+    }
+  },
+  methods: {
+    getCheckedDatum(data) {
+      this.checkedDatum = data
+    }
+  },
   render(h) {
+    const { getCheckedDatum, checkedDatum } = this
     return (
       <div>
         <Markdown src={require('./doc.md')} />
         <div>
-          <Checkbox.Group>
+          <code>
+            {JSON.stringify(checkedDatum)}
+          </code>
+        </div>
+        <br />
+        <div>
+          <Checkbox.Group onChange={getCheckedDatum}>
             <Checkbox name="all" checked={true}>All</Checkbox>
             <Checkbox name="1">Pikachu</Checkbox>
             <Checkbox name="2">Eevee</Checkbox>
