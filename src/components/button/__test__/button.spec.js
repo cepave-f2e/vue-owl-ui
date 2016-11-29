736d307c8958498f2f31480e0a390885bc0a9963
Kelly Su <yoyo420303@gmail.com>
2016-11-29 14:27:36 +0800

    [OWL-597] <Button.Group> test specs

--- src/components/button/__test__/button.spec.js
+++ src/components/button/__test__/button.spec.js
@@ -25,3 +25,59 @@ it('set `disabled` props', () => {
   expect(vm.style[0]).toContain('button')
   expect(vm.style[1]).toContain('disabled')
 })
+
+it('test <Button.Group> with props `options`', () => {
+  const options = [
+    { value: 'winnie', title: 'Winnie The Pooh' },
+    { value: 'piglet', title: 'Piglet' },
+    { value: 'tigger', title: 'Tigger', selected: true }
+  ]
+  const getButtonGroupData = (data) => {}
+  const vm = shallow({
+    render(h) {
+      return (
+        <Button.Group options={options} onChange={getButtonGroupData} />
+      )
+    }
+  })
+  expect(vm.buttonOptions).toEqual([
+    { value: 'winnie', title: 'Winnie The Pooh' },
+    { value: 'piglet', title: 'Piglet' },
+    { value: 'tigger', title: 'Tigger', selected: true }
+  ])
+  expect(vm.selectedIdx).toBe(2)
+})
+
+it('click on <Button.Group> to switch chosen option', async() => {
+  let vm
+  const options = [
+    { value: 'winnie', title: 'Winnie The Pooh' },
+    { value: 'piglet', title: 'Piglet' },
+    { value: 'tigger', title: 'Tigger', selected: true }
+  ]
+  const getButtonGroupData = (data) => {}
+  await new Promise((done) => {
+    vm = shallow({
+      render(h) {
+        return (
+          <Button.Group options={options} onChange={getButtonGroupData} />
+        )
+      }
+    })
+    $(vm.$children[2].$el).trigger('click')
+    expect(vm.selectedIdx).toBe(2)
+    expect(vm.buttonOptions).toEqual([
+      { value: 'winnie', title: 'Winnie The Pooh' },
+      { value: 'piglet', title: 'Piglet' },
+      { value: 'tigger', title: 'Tigger', selected: true }
+    ])
+    $(vm.$children[0].$el).trigger('click')
+    vm.$nextTick(done)
+  })
+  expect(vm.selectedIdx).toBe(0)
+  expect(vm.buttonOptions).toEqual([
+    { value: 'winnie', title: 'Winnie The Pooh', selected: true },
+    { value: 'piglet', title: 'Piglet', selected: false },
+    { value: 'tigger', title: 'Tigger', selected: false }
+  ])
+})
