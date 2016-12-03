2a439fdf3ce7889cb91ff02e4ba0067150d9b078
RoCky Wu <rocky823@gmail.com>
2016-12-03 12:09:46 +1100

    Change to use `shelljs`

--- scripts/github-publish.js
+++ scripts/github-publish.js
@@ -1,18 +1,12 @@
-import { exec } from 'child_process'
-import { version, repository } from '../package.json'
+import 'shelljs/global'
+import pkg, { version, repository } from '../package.json'
 
 const ver = `v${version}`
 
-exec(`cd npm && 
-git init && 
-git remote add origin ${repository.url.replace(/^git\+/, '')} && 
-git add . && 
-git commit -anm "${ver}" &&
-git checkout -b ${ver} &&
-git push origin ${ver}`, (err, stdout) => {
-  if (err) {
-    return console.log(err)
-  }
-
-  console.log(stdout)
-})
+cd('npm')
+exec(`git init`)
+exec(`git remote add origin ${repository}`)
+exec(`git add .`)
+exec(`git commit -anm "${ver}"`)
+exec(`git checkout -b ${ver}`)
+exec(`git push origin ${ver}`)
