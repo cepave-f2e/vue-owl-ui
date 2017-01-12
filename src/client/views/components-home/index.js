6961902043fe22e74f068e465ebe41a8672be74b
RoCky Wu <rocky823@gmail.com>
2017-01-12 20:44:24 +1100

    Added `Custom Primary Color` doc and remove `OWL Daily`

--- src/client/views/components-home/index.js
+++ src/client/views/components-home/index.js
@@ -1,6 +1,4 @@
-import Loading from '~com/loading'
-import axios from 'axios'
-import owlSrcs from './owl-src.json'
+import Markdown from '../../components/markdown'
 
 const CompnentHomePage = {
   name: 'CompnentHomePage',
@@ -12,69 +10,16 @@ const CompnentHomePage = {
   },
 
   methods: {
-    renderImg(src) {
-      const img = new Image()
-      img.src = src
 
-      img.onload = () => {
-        this.owlSrc = src
-        this.loading = false
-        localStorage.__owlSrc = src
-      }
-
-      img.onerror = () => {
-        const owl = owlSrcs[Math.floor(Math.random() * owlSrcs.length)]
-        this.renderImg(owl.src)
-      }
-    }
   },
 
   mounted() {
-    const { renderImg } = this
-    const date = new Date().getDate()
 
-    if (+localStorage.__owlDate === date) {
-      renderImg(localStorage.__owlSrc)
-    } else {
-      const owl = owlSrcs[Math.floor(Math.random() * owlSrcs.length)]
-      renderImg(owl.src)
-
-      localStorage.__owlDate = date
-    }
-
-    return
-
-    axios({
-      url: 'https://api.cognitive.microsoft.com/bing/v5.0/images/search',
-      params: {
-        q: 'owl',
-        count: 150,
-        offset: Math.floor(Math.random() * 1000),
-      },
-      headers: { 'Ocp-Apim-Subscription-Key': '' }
-    })
-    .then((res) => {
-      const owlsrcs = res.data.value.map((owl) => {
-        const [r] = decodeURIComponent(owl.contentUrl).match(/r=http[^&]+/)
-
-        return {
-          thumb: owl.thumbnailUrl,
-          src: r.replace(/^r=https?:/, ''),
-          width: owl.width,
-          height: owl.height,
-        }
-      })
-    })
   },
   render(h) {
-    const { owlSrc, loading } = this
     return (
       <div>
-        <Loading show={loading} />
-        <h1 style={{ fontFamily: 'Architects Daughter' }}> OWL Daily </h1>
-        <p>
-          <img style={{ maxWidth: '100%' }} src={owlSrc} />
-        </p>
+        <Markdown src={require('./doc.md')} />
       </div>
     )
   }
