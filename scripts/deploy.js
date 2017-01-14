f90f6c68028a304230e7d193b593747951c08b1e
RoCky Wu <rocky823@gmail.com>
2017-01-14 20:01:22 +1100

    Switch to use Travis

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,56 @@
-const { repository, version } = require('../package.json')
-const { GH_TOKEN } = process.env
-const tokenRepo = repository.replace(/(github.com)/, `${GH_TOKEN}@$1`)
+const { version, repository } = require('../package.json')
+require('shelljs/global')
 
-module.exports = {
-  ver: version,
-  tag: `v${version}`,
-  repo: tokenRepo,
+const { TRAVIS_BRANCH, TRAVIS_MATRIX, TRAVIS_PULL_REQUEST_BRANCH,
+  GH_TOKEN, NPM_PASSWD } = process.env
+
+const tokenRepo = repository.replace(/(github.com)/, `${GH_TOKEN}@$1`)
+const tag = `v${version}`
+
+console.log({ TRAVIS_BRANCH, TRAVIS_MATRIX, TRAVIS_PULL_REQUEST_BRANCH })
+
+if (TRAVIS_MATRIX === 'test') {
+  exec('curl -s https://codecov.io/bash | bash')
+
+  if (TRAVIS_BRANCH === 'dev') {
+    // Clean pr branch
+    const commit = exec(`git log --pretty=format:"%s" -1`, {
+      silent: true
+    }).stdout
+
+    const prBranch = /^Merge pull request #\d+ from cepave-f2e\/(.+)/.test(commit) && RegExp.$1
+
+    if (prBranch) {
+      exec(`git push ${tokenRepo} ${prBranch} --delete`)
+    }
+  }
+}
+
+if (TRAVIS_BRANCH === 'master') {
+  if (TRAVIS_MATRIX === 'build.ui') {
+    // Add GH Tag
+    exec(`git tag ${tag}`)
+    exec(`git push ${tokenRepo} ${tag}`, {
+      silent: true,
+    })
+
+    // Publish to NPM
+    exec(`echo -e "cepave\n${NPM_PASSWD}\nrwu@cepave.com" | npm login`)
+    exec(`npm publish ./npm --access=public`)
+  }
+
+  if (TRAVIS_MATRIX === 'build.pages') {
+    exec(`git config --global user.email "auto_deploy@circleci.com"`)
+    exec(`git config --global user.name "CircleCI"`)
+
+    // Publish to gh-pages
+    cd('gh-pages')
+    exec('git init')
+    exec(`git remote add origin ${tokenRepo}`)
+    exec('git add .')
+    exec(`git checkout -b gh-pages`)
+    exec(`git commit -anm '${version}'`)
+    exec(`git push origin gh-pages -f`)
+  }
+
 }
