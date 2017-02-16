755208e5a62f4d30f8b8e2f7629fc9d2c886fe29
Kelly Su <yoyo420303@gmail.com>
2017-02-16 11:13:52 +0800

    [MultiSelect] delete Label by backspace - test specs

--- src/components/multi-select/__test__/multi-select.spec.js
+++ src/components/multi-select/__test__/multi-select.spec.js
@@ -238,12 +238,12 @@ it('test <MultiSelect /> keyboard event', async() => {
   expect(vm.disablePointer).toBe(false)
 })
 
-it('test <MultiSelect /> delete Label feature', async() => {
+it('test <MultiSelect /> delete Label by clicking', async() => {
   let vm
   const selectedItems = [0, 1]
   const pokemon = [
     { value: 'Piglet', id: 23 },
-    { title: 'Raticate', value: 'raticate', id: 18 },
+    { value: 'raticate', id: 18 },
     { value: 'pikachu', id: 24 },
   ]
   await new Promise((done) => {
@@ -258,4 +258,42 @@ it('test <MultiSelect /> delete Label feature', async() => {
   })
   $(vm.$el.children[0].children[0].children[0]).trigger('click')
   expect(vm.selectedIdx).toEqual([1])
+})
+
+it('test <MultiSelect /> delete Label by backspace', async() => {
+  let vm
+  const selectedItems = [2, 1, 0]
+  const pokemon = [
+    { value: 'Piglet', id: 23 },
+    { value: 'raticate', id: 18 },
+    { value: 'pikachu', id: 24 },
+  ]
+
+  await new Promise((done) => {
+    vm = shallow({
+      render(h) {
+        return (
+          <MultiSelect displayKey="value" selectedIdx={selectedItems} options={pokemon} />
+        )
+      }
+    })
+    vm.$nextTick(done)
+  })
+
+  const key8 = new window.KeyboardEvent('keydown', { keyCode: 8, which: 8 })
+
+  await new Promise((done) => {
+    vm.$el.children[0].children[1].dispatchEvent(key8)
+    vm.$nextTick(done)
+  })
+
+  expect(vm.focusedLabelIdx).toBe(selectedItems.length - 1)
+
+  await new Promise((done) => {
+    vm.$el.children[0].children[1].dispatchEvent(key8)
+    vm.$nextTick(done)
+  })
+
+  expect(vm.selectedIdx).toEqual([2, 1])
+  expect(vm.focusedLabelIdx).toBe(-1)
 })
