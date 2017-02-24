93e80b2142819f76349f5d80a19d2ec1a0683f65
RoCky Wu <rocky823@gmail.com>
2017-02-24 18:05:19 +1100

    Update API and  doc of <TimePicker />

---
+++ src/components/date-picker/__test__/date-format.spec.js
@@ -0,0 +1,15 @@
+import dateFormat from '../date-format'
+
+const D = new Date(2017, 3 - 1, 2)
+
+describe('Test Date Format', () => {
+  it('Convert format with prefix 0', () => {
+    expect(dateFormat(D, 'yyyy/mm/dd')).toBe('2017/03/02')
+    expect(dateFormat(D, 'yyyy-mm-dd')).toBe('2017-03-02')
+    expect(dateFormat(D, 'dd/mm/yyyy')).toBe('02/03/2017')
+  })
+
+  it('test format `yy/m/d`', ()=> {
+    expect(dateFormat(D, 'yy/m/d')).toBe('17/3/2')
+  })
+})
