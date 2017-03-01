059ed2249fb4ac2b802cfa154cbd4ec27992158e
RoCky Wu <rocky823@gmail.com>
2017-03-01 19:12:41 +1100

    Added DatePicer spec and doc

--- src/components/date-picker/index.js
+++ src/components/date-picker/index.js
@@ -1,6 +1,7 @@
-import Icon from '~com/icon'
+import Icon from '../icon'
 import s from './date-picker.scss'
 import Cal from './cal'
+import dateFormat from './date-format'
 
 const DatePicker = {
   name: 'DatePicker',
@@ -11,23 +12,82 @@ const DatePicker = {
       default: () => [(new Date).getFullYear(), (new Date).getMonth() + 1]
     },
 
-    range: {
+    format: {
+      type: String,
+      default: 'yyyy/mm/dd',
+    },
+
+    min: {
+      type: Date,
+    },
+
+    max: {
+      type: Date,
+    },
+
+    firstDayOfWeek: {
+      type: Number,
+      default: 1,
+    },
+
+    colorfulWeekend: {
       type: Boolean,
       default: false,
     },
 
+    open: {
+      type: Boolean,
+      default: false
+    },
+
+    defaultValue: {
+      type: String,
+      default: '',
+    },
+  },
+
+  watch: {
+    open(open) {
+      const { $el } = this
+      if (open) {
+        $el.focus()
+      } else {
+        $el.blur()
+      }
+    },
+    defaultValue(value) {
+      this.value = value
+    }
   },
 
   data() {
     const [year, month] = this.yearMonth
+
+    const { defaultValue, format } = this
     return {
       year,
       month,
+      value: defaultValue || dateFormat(new Date(), this.format)
     }
   },
 
   methods: {
+    handlePick(d) {
+      const { format, $el } = this
+      this.value = dateFormat(d.Date, format)
 
+      $el.blur()
+      this.$emit('pick', {
+        value: this.value,
+        ...d
+      })
+    },
+    onNext(d) {
+      this.$emit('next', d)
+    },
+    onPrev(d) {
+      this.$emit('prev', d)
+    }
   },
 
   computed: {
@@ -41,11 +101,25 @@ const DatePicker = {
   },
 
   render(h) {
-    const { classes, } = this
+    const { classes, yearMonth, min, max, firstDayOfWeek, colorfulWeekend, value,
+    handlePick, onNext, onPrev } = this
+
     return (
-      <div class={[s.picker, classes]}>
-        <Icon typ="date" class={[s.cal]} />
-        <Cal min={new Date('2017/1/1')} max={new Date(`2017/3/20`)} />
+      <div class={[s.picker, classes]} tabIndex="-1">
+        <div class={[s.input]}>
+          <Icon typ="date" class={[s.cal]} />
+          <span class={[s.value]}>
+            { value }
+          </span>
+        </div>
+        <div>
+          <Cal yearMonth={yearMonth} min={min} max={max}
+            firstDayOfWeek={firstDayOfWeek}
+            onPick={handlePick}
+            onNext={onNext}
+            onPrev= {onPrev}
+            colorfulWeekend={colorfulWeekend} />
+        </div>
       </div>
     )
   }
